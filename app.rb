41b3a4fa57c255e7cdaaa9d0cfe91fd52ef80e1b
Elrey Belmonti <elbelmonti@gmail.com>
2017-09-15 10:32:09 -0700

    display home page with starting words

---
+++ app.rb
@@ -0,0 +1,23 @@
+require 'sinatra'
+require 'sinatra/reloader'
+also_reload 'lib/**/*.rb'
+require './lib/word'
+require 'pry'
+
+get('/') do
+  start_word1 = Word.new("apple")
+  start_word1.add_definition("a fruit")
+  start_word1.save
+
+  start_word2 = Word.new("play")
+  start_word2.add_definition("to enjoy one time with an activity")
+  start_word2.save
+
+  start_word3 = Word.new("snow")
+  start_word3.add_definition("water in a solid form")
+  start_word3.save
+  
+  @words = Word.all
+  binding.pry
+  erb(:home)
+end
--- spec/word_intergration_spec.rb
+++ spec/word_intergration_spec.rb
@@ -1,11 +1,16 @@
-# require 'capybara/rspec'
-# require './app'
-#
-# Capybara.app = Sinatra::Application
-# set(:show_exceptions, false)
-#
-# describe('word', {:type => :feature}) do
-#   before do
-#
-#   end
-# end
+require 'capybara/rspec'
+require './app'
+
+Capybara.app = Sinatra::Application
+set(:show_exceptions, false)
+
+describe('word', {:type => :feature}) do
+  before do
+    Word.clear
+  end
+
+  it 'displays words on page' do
+    visit('/')
+    expect(page).to have_content("apple play snow")
+  end
+end
