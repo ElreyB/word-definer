5f3672e4a2e30bb94d66f41588b1673420576c1b
Elrey Belmonti <elbelmonti@gmail.com>
2017-09-16 13:51:40 -0700

    add input error when user enters an empty string and refactor well design

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,3 +1,13 @@
 $(document).ready(function(){
-  
+  $("button.add, button.delete").click(function(e){
+    formGroup = $(this).siblings(".form-group")
+    var userInput = formGroup.find("input").val();
+
+    if (userInput === ""){
+      e.preventDefault();
+      formGroup.addClass("has-error");
+      $("div.error").removeClass("hide");
+      return;
+    }
+  });
 });
